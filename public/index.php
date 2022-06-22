ff52f5ca3807e2f243a2f6a2bfe8486978f4c91e
Olintho Jesus <olinthojesus@yahoo.com.br>
2022-06-22 17:21:51 -0300

    Adding class User

--- public/index.php
+++ public/index.php
@@ -4,16 +4,9 @@ echo "<h1>InNOut</h1>";
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
-$user = new User();
+echo User::getSelect(['name' => 'Chaves'], 'id, colums1');
+echo "<br>";
+echo User::getSelect();
+
+
 echo "fim";
-
-$sql = "SELECT * FROM users";
-$result = Database::getResultFromQuery($sql);
-
-while ($row = $result->fetch_assoc()) {
-    foreach ($row as $key => $value) {
-        echo $key . ": " . $value . ", ";
-    }
-    echo "<br>";
-    echo 'fim';
-}
--- src/models/Model.php
+++ src/models/Model.php
@@ -1,5 +1,63 @@
 <?php
 
-class Model {
-    
+class Model
+{
+
+    protected static $tableName = '';
+
+    protected static $columns = [];
+
+    protected $values = [];
+
+    function __construct($arr)
+    {
+        $this->loadFromArray($arr);
+    }
+
+    public function loadFromArray($arr)
+    {
+        if ($arr) {
+            foreach ($arr as $key => $value) {
+                $this->$key = $value;
+            }
+        }
+    }
+
+    public function __get($key)
+    {
+        return $this->values[$key];
+    }
+
+    public function __set($key, $value)
+    {
+        $this->values[$key] = $value;
+    }
+
+    public static function getSelect($filters = [], $columns = '*')
+    {
+        $sql = "SELECT ${columns} FROM " . static::$tableName . static::getFilters($filters);
+        return $sql;
+    }
+
+    private static function getFilters($filters)
+    {
+        $sql = '';
+        if (count($filters) > 0) {
+            $sql .= " WHERE 1 = 1";
+            foreach ($filters as $column => $value) {
+                $sql .= " AND ${column} = " . static::getFormattedValue($value);
+            }
+        }
+        return $sql;
+    }
+
+    private static function getFormattedValue($value)
+    {
+        if (is_null($value)) {
+            return "null";
+        } elseif (gettype($value === 'string')) {
+            return "'${value}'";
+        }
+    }
 }
+    
