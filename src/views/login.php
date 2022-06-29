bc047bba323d8f2c0d5c91b574bd4e94a3853124
Olintho Jesus <olinthojesus@yahoo.com.br>
2022-06-29 09:20:10 -0300

    Finishing Authentication Screen.

--- src/views/login.php
+++ src/views/login.php
@@ -1,22 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="${encoding}">
-	<meta name="viewport" content="width-device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" href="assets/css/comum.css">
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="assets/css/icofont.min.css">
-	<link rel="stylesheet" href="assets/css/login.css">
-	<title>In 'n Out</title>
+<meta charset="${encoding}">
+<meta name="viewport" content="width-device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="assets/css/comum.css">
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/icofont.min.css">
+<link rel="stylesheet" href="assets/css/login.css">
+<title>In N' Out</title>
 </head>
 <body>
-	<form action="#" method="post">
-	
-	<!-- HEREIN: OSJ Cap. 252 - 3:36 Tela de autenticação -->
-	</form>
-	
+	<form class="form-login" action="#" method="post">
+		<div class="login-card card">
 
-</div>
+			<div class="card-header">
+				<i class="icofont-travelling mr-2"></i> 
+				<span class="font-weight-light">In</span> 
+				<span class="font-weight-bold mx-2">N' </span> 
+				<span class="font-weight-light">Out</span> 
+				<i class="icofont-runner-alt-1 ml-2"></i>
+			</div>
+
+			<div class="card-body">
+				<div class="form-group">
+					<label for="email">E-mail</label> <input type="email" id="email"
+						class="form-control" placeholder="Digite o seu e-mail" autofocus>
+				</div>
+				<div class="form-group">
+					<label for="password">Senha</label> <input type="password"
+						id="password" class="form-control" placeholder="Digite a senha">
+				</div>
+
+			</div>
+			<div class="card-footer">
+				<button class="btn btn-lg btn-primary">Entrar</button>
+			</div>
+
+		</div>
+	</form>
+	<!-- HEREIN: OSJ Cap. 253 - Login #01 -->
+
+
+
+	</div>
 </body>
 </html>
